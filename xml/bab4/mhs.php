13bc302ff1aec9c8b387a71aba2b4c1c6e5b72fc
adisatria <adisatria1993@gmail.com>
2014-10-01 08:23:38 +0700

    add mhs

---
+++ xml/bab4/mhs.php
@@ -0,0 +1,33 @@
+<?php
+	header('content-type : text/xml; charset=ISO-8859-1');
+	include "koneksi.php";
+	
+	//cek path element
+	$path = $_SERVER[PATH_INFO];
+	if ($path != null){
+		$path_params = spliti("/", $path);
+		}
+		
+	//metode request untuk GET
+	if($_SERVER['REQUEST_METHOD'] == 'GET') {
+		if($path_params[1] != null){
+		 $query = "select nim, nama, alamat, prodi from mahasiswa where nim = $path_params[1]";
+		 }
+		 else{
+		 $query = "select nim, nama, alamat, prodi from mahasiswa";
+		 }
+		 $result = mysql_query($query) or die ('query failed : '.mysql_error());
+		 
+		 echo "<data>";
+		 while ($line = mysql_fetch_array($result, MYSQL_ASSOC)){
+		 echo "<mahasiswa>";
+		 foreach ($line as $key => $col_value){
+		 echo "</data>";
+		 
+		 mysql_free_result($result);
+		 }
+		 
+		 //request untuk delete
+		 }
+		 mysql_close($link);
+?>
