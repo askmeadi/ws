12950a92263c9d26035f1caebd2edb4389b2bf30
askmeadi <adidatria1993@gmail.com>
2014-10-08 08:21:19 +0700

    add nusoap

--- xml/bab4/server.php
+++ xml/bab4/server.php
@@ -1,7 +1,7 @@
 <?php
 	//call library
 	require_once('nusoap/lib/nusoap.php');
-	require_once('adodb/adodb/adodb.inc.php');
+	require_once('adodb/adodb.inc.php');
 	
 	$server = new nusoap_server;
 	$server->configureWSDL('server', 'urn:server');
@@ -11,8 +11,9 @@
 	$server->register('login_ws',
 	array('username' => 'xsd:string',
 	'password' => 'xsd: string'), //output
+	array('return' => 'xsd: string'),
 	'urn:server', //namespace
-	'urn:server#loginserver', //soapaction
+	'urn:server#loginServer', //soapaction
 	'rpc', //style
 	'encoded', //use
 	'login'); //description
@@ -23,9 +24,10 @@
 	$password = md5($password);
 	//buat koneksi
 	$db = NewADOConnection('mysql');
-	$db -> connect('localhost', 'root', '', 'data_mahasiswa');
+	$db->connect('localhost', 'root', '', 'data_mahasiswa');
 	//cek username dan password dari db
-	$sql = $db -> Execute("SELECT * FROM user WHERE username='$username' AND password = '$password'");
+	$sql = $db->Execute("SELECT * FROM user WHERE username = '$username' AND password = '$password'");
+	print_r($sql); die();
 	
 	//cek adanya user dan password di db
 	if($sql->RecordCount() >= 1)
@@ -37,7 +39,7 @@
 	}
 	
 	//create HTTP listener
-	$HTTP_RAW_POST_DATA = isset($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA :'';
+	$HTTP_RAW_POST_DATA = isset($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA : '';
 	$server->service($HTTP_RAW_POST_DATA);
 	
 ?>
