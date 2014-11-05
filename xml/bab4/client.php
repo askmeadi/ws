ed818c6793abe97f451c5f2964386dd64153b560
askmeadi <adisatria1993@gmail.com>
2014-11-05 08:43:50 +0700

    add

--- xml/bab4/client.php
+++ xml/bab4/client.php
@@ -4,12 +4,12 @@
 	//panggil library
 	require_once('nusoap/lib/nusoap.php');
 	//mendefinisikan alamat url service yang disediakan oleh client
-	$client = new nusoap_client('http://localhost/SIT/ws.git/xml/bab4/server.php?wsdl', true);
+	$client = new nusoap_client('http://localhost/SIT/ws.git/xml/bab4/server.php?wsdl', 'WSDL');
 	$username = isset($_POST["username"]) ? $_POST["username"] : 'admin';
 	$password = isset($_POST["password"]) ? $_POST["password"] : 'admin';
 	$result = $client->call('login_ws', array('username'=>$username, 'password'=>$password));
 	
-	if($result == "Login Berhasil"){
+	if($result == "Login berhasil"){
 		$_SESSION['username'] = $username;
 		header("location:index.php");
 		}else{
