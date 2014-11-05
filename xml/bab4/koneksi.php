df1b3a07de375cedacce1aa1d221e69591040d3d
askmeadi <adisatria1993@gmail.com>
2014-11-05 08:20:54 +0700

    add koneksi

--- xml/bab4/koneksi.php
+++ xml/bab4/koneksi.php
@@ -1,5 +1,5 @@
 <?php
 	$link = mysql_connect('localhost', 'root', '')
 	or die('could not connect : '.mysql_error());
-	mysql_select_db('data mahasiswa') or die ('could not select database');
+	mysql_select_db('data_mahasiswa') or die ('could not select database');
 ?>
